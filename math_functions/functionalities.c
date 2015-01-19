7368ccc15babff5fb884988b114d244b3b4a0e6c
Tacio Fontes Belmonte <p13243353@myemail.dmu.ac.uk>
2015-01-19 13:26:06 +0000

    creation of math_functions directory

---
+++ math_functions/functionalities.c
@@ -0,0 +1,24 @@
+#include <stdio.h>
+#include <stdlib.h>
+
+void main(){
+	int num1, num2, sum;
+
+	//Add
+	printf("Type a number");
+	scanf("%d", &num1);
+	printf("Type another number");
+	scanf("%d", &num2);
+	
+	sum = num1+num2;
+	printf("Sum = %d", sum);	
+
+	//Subtraction
+	//result = num1-num2;
+
+	//Multiply
+	//result = num1*num2;
+
+	//Divide
+	result = num2/num1;
+}	
