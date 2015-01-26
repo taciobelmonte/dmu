a97ad5146ee33141c3d838f7300238177099a5c0
Tacio Fontes Belmonte <p13243353@myemail.dmu.ac.uk>
2015-01-26 13:33:37 +0000

    new folder

---
+++ functionalities.c.BACKUP.5123.c
@@ -0,0 +1,11 @@
+<<<<<<< HEAD
+<<<<<<< HEAD
+//This is the line causing issue in the master branch
+=======
+
+//This is the second line causing issue in the master branch
+>>>>>>> conflict
+=======
+//This is the line causing issue in the conflict branch
+//This is the second line causing issue in the conflict branch
+>>>>>>> conflict
---
+++ functionalities.c.LOCAL.5123.c
@@ -0,0 +1,6 @@
+<<<<<<< HEAD
+//This is the line causing issue in the master branch
+=======
+
+//This is the second line causing issue in the master branch
+>>>>>>> conflict
